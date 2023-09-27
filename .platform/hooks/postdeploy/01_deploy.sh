343253b1ce9138fa64be911a5825b2942b06286f
Eddy <eddyndizera@gmail.com>
2023-09-27 18:30:56 +0200

    modify hook

--- .platform/hooks/postdeploy/01_deploy.sh
+++ .platform/hooks/postdeploy/01_deploy.sh
@@ -4,9 +4,7 @@ source /var/app/venv/*/bin/activate && {
 
 # collecting static files
 python manage.py collectstatic --noinput;
-# log which migrations have already been applied
-python manage.py showmigrations;
-# migrate the rest
+# migrate
 python manage.py migrate --noinput;
 # another command to create a superuser (write your own)
 python manage.py refresh_partners;
